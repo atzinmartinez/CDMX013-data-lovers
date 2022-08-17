bec7bfebee952f249c7ff4b79cae9a2cf393a328
Ale <ale.ga.ca90@gmail.com>
2022-08-17 17:29:46 -0500

    Cambio nombre boton

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+<<<<<<< Updated upstream
 import data from './data/ghibli/ghibli.js';
 console.log(data.films[0].title);
 //console.log(data);
+=======
+//import { } from './data/ghibli/ghibli.js';
+
+import data from './data/ghibli/ghibli.js';{
+  //  console.log(data);
+const arrayObjects= data[0]
+console.log(arrayObjects)
+    
+
+
+
+}
+
+
+//mostrar 1 solo titulo de plicula en la web
+//tomar imagenes de posters para mostrarlas al usuario
+//tomar titulos y años de peliculas primer HU1
+>>>>>>> Stashed changes
