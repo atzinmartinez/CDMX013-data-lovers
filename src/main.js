298a1df197e692d0963289bdcd3e60f7db088c98
atzinmartinez <atzinrgm@gmail.com>
2022-08-19 10:50:45 -0500

    ghiblidata1

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import allData from './data/ghibli/ghibli.js';
-const history = document.getElementById ("history")
-history.classList="filmStyle"
+const history = document.getElementById("history")
+history.classList = "filmStyle"
 const film = allData.films
-const generateHTML = (film) =>{     //arrow function
-const div = document.createElement ("div") //?
-const titleFilm = document.createElement ("h2") // nombre pelicula
-titleFilm.textContent=film.title
-const poster = document.createElement ("img") // imagen pelicula
-poster.setAttribute("src", film.poster)
-const release_date = document.createElement ("h4") //año pelicula
-release_date.textContent = film.release_date
-div.append(poster, titleFilm, release_date)
-return div
+const generateHTML = (film) => {     //arrow function
+    const div = document.createElement("div") //?
+    const titleFilm = document.createElement("h2") // nombre pelicula
+    titleFilm.textContent = film.title
+    const poster = document.createElement("img") // imagen pelicula
+    poster.setAttribute("src", film.poster)
+    const release_date = document.createElement("h4") //año pelicula
+    release_date.textContent = film.release_date
+    div.append(poster, titleFilm, release_date)
+    return div
 }
-film.forEach(oneFilm=>history.appendChild(generateHTML(oneFilm)))
-//console.log(allData);
-console.log(film [0].title);
-//console.log(data.films[0].title);
+film.forEach(oneFilm => history.appendChild(generateHTML(oneFilm)))
+
+console.log(film[0].title);
