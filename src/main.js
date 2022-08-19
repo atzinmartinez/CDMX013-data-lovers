a3c1229e6cb3f476dd7df4190afcc386c5566d52
atzinmartinez <atzinrgm@gmail.com>
2022-08-19 10:32:08 -0500

    dataghibli

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-import data from './data/ghibli/ghibli.js';
-console.log(data.films[0].title);
-//console.log(data);
+import allData from './data/ghibli/ghibli.js';
+const history = document.getElementById ("history")
+history.classList="filmStyle"
+const film = allData.films
+const generateHTML = (film) =>{     //arrow function
+const div = document.createElement ("div") //?
+const titleFilm = document.createElement ("h2") // nombre pelicula
+titleFilm.textContent=film.title
+const poster = document.createElement ("img") // imagen pelicula
+poster.setAttribute("src", film.poster)
+const release_date = document.createElement ("h4") //año pelicula
+release_date.textContent = film.release_date
+div.append(poster, titleFilm, release_date)
+return div
+}
+film.forEach(oneFilm=>history.appendChild(generateHTML(oneFilm)))
+//console.log(allData);
+console.log(film [0].title);
+//console.log(data.films[0].title);
