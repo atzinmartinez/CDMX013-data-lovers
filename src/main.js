a073683bd0ba4d0601646976ee28cf1f37448067
atzinmartinez <atzinrgm@gmail.com>
2022-08-29 14:48:47 -0500

    data filtrada sin codigo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-
-
+import { filterByDirector, filterDate } from './data.js';
 import allData from './data/ghibli/ghibli.js';
 
 //import {firstDirector} from './data.js'
@@ -15,6 +14,11 @@ const generateHTML = (film) => {     //arrow function
     const poster = document.createElement("img") // imagen pelicula
     poster.setAttribute("src", film.poster)
     poster.classList.add('imagen');
+
+    const ranking = document.createElement ("h1")
+    ranking.textContent = film.rt_score
+
+
     const release_date = document.createElement("h4") //año pelicula
     release_date.textContent = film.release_date
     div.append(poster, titleFilm, release_date)
@@ -24,82 +28,22 @@ film.forEach(oneFilm => container.appendChild(generateHTML(oneFilm))) //con la m
 //empaquetar una funcion
 
 
-//film.forEach(filterDirector => history.appendChild(generateHTML(filterDirector)))
-
-
 const filterDirector = document.getElementById("filterDirector")
-filterDirector.addEventListener("click", function(e){
-const firstDirector= film.filter(item=>item.director === e.target.value);
-console.log(firstDirector);
+filterDirector.addEventListener("click", function(director){
+container.innerHTML = ""
+filterByDirector (director.target.value).forEach(oneFilm => container.appendChild(generateHTML(oneFilm))) 
+ })
 
+ /*const ranking = document.createElement ("h1")
+ ranking.textContent = film.rt_score */
 
-container.innerHTML= generateHTML
-
-console.log(container.innerHTML);
-
-firstDirector.filter(onefilter => container.appendChild(generateHTML (onefilter)))
-
-
-//return firstDirector.forEach(onefilter => container.appendChild(generateHTML (onefilter)))
-
-
-
-// container.generateHTML = ""
-
- //como pintar en la interfaz
-
+ const filterDate = document.getElementById("filterDate")
+ filterDate.addEventListener("click", function(ranking){
+  //console.log ("Hola");
+ container.innerHTML = ""
+ filterByRanking (ranking.target.value).forEach(oneFilm => container.appendChild(generateHTML(oneFilm))) 
  
-
-
-
-
-  console.log(firstDirector)
-  console.log(e.target.value)
-  
-// crear un contenedor para arrojar el filtrado containerCards.innerHTML =""
-  //const filterRanking = (director, data ) => data.filter (films => films.director == director);
-   
-
-//console.log(firstDirector)
-
   })
 
-
-
-  // ranking
-
-
-  //export const filterDirector = (director, data ) => data.filter (films => films.director == director);
-  // let= "Hayao Miyazaki"
-//let hayaoMiyazaki = arreglo.filter(element => element > 10);
-   /*
-   let director = [films[director]];
-   arrayofObjetcts.forEach(object=>{
-    console.log(object)
-   })
-   
-     var currentNumber = numbers[i];
-   
-     if (currentNumber > 10) {
-   
-       greaterTen.push(currentNumber)
-   
-     }
-   
-   }
-   
-   
-   console.log(greaterTen); 
-
-
-
-const btnDirector = document.getElementById ("director")
-
-btnDirector.addEventListener("click", function showDirector) // Escucha click (evento)
-
-  function showDirector (){
-
-        const director = document.createElement ("h1")
-    director.textContent = film.director
-    console.log(director)
-}9 */
+ // DOM
+ //rt_score
