7e64dfa89f9b9695da786253f0b676720f9715ba
atzinmartinez <atzinrgm@gmail.com>
2022-08-30 19:28:44 -0500

    HU2 terminada

--- src/data.js
+++ src/data.js
@@ -10,17 +10,25 @@ export const filterByDirector = (nameDirector)=>{
 }
 }
 
-export const filterDate = (filterDate)=>{
-  if (filterDate == "Escoger Fecha") { 
-  return film
 
+// elementos mayores a 1980 un if 1980 else 1990 > 1980
+// elementos mayores a 1990 un if 2000 else 2010
+// elementos mayores a 2000
+// elementos mayores a 2010
+
+
+//recibe un año y nos regresa peliculas 
+export const filterByDate = (date)=> {
+  if (date == "escogerFecha") { 
+    return film
   } else {
-  return film.filter(item=>item.release_date == "1989")
-  
-}
+return film.filter (film => film.release_date <= date && film.release_date >= (date - 10) )
+} }
 
-}
-console.log (filterDate)
 
-// funciones
+
+
+
+
+
 
